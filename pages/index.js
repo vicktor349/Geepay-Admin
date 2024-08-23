ee9fe46f89f2b8626f326b8bd2b42ac40f2e607f
vicktor349 <ayomidevictor349@gmail.com>
2024-08-23 12:51:35 +0100

    Mobile responsivity

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import SideNavbar from "@/components/SideNavbar";
+import Sales from "@/components/Sales";
+import Sale from "@/components/Sale";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,11 +12,17 @@ export default function Home()
   return (
     <>
       <Head>
-        <title>GeePay | Home</title>
+        <title>Admin | Home</title>
       </Head>
-      <main className={`${inter.className}`}>
+      <main className={`${inter.className} relative`}>
         <Navbar />
         <SideNavbar />
+        <div className="absolute sssm:left-[8px] ssms:left-[10px] sm:left-[90px] md:left-[100px] lg:left-[110px] xl:left-[80px] top-36 xl:w-[calc(100%-80px)] flex justify-center">
+          <div className="flex justify-center w-full flex-col xl:flex-row xl:space-x-6">
+            <Sales />
+            <Sale />
+          </div>
+        </div>
       </main>
     </>
   );
