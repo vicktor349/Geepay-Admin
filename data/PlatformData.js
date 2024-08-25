60d9c1b4dee0449f94a6b0adbd162fdf2df0ffe3
vicktor349 <ayomidevictor349@gmail.com>
2024-08-25 17:59:43 +0100

    Plaform data

---
+++ data/PlatformData.js
@@ -0,0 +1,32 @@
+const platformData = [
+    {
+        "id": 1,
+        name: "Book Bazaar",
+        price: "2,500,000",
+        percentage: "15",
+        value: 48
+    },
+    {
+        "id": 2,
+        name: "Artisan Aisle",
+        price: "1,800,000",
+        percentage: "10",
+        value: 78
+    },
+    {
+        "id": 3,
+        name: "Toy Troop",
+        price: "1,200,000",
+        percentage: "8",
+        value: 50
+    },
+    {
+        "id": 4,
+        name: "XStore",
+        price: "600,000",
+        percentage: "5",
+        value: 98
+    },
+]
+
+export default platformData;
