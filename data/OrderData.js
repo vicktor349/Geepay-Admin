cb97ede03fab664b2c108ddfc32b59d812f52e17
vicktor349 <ayomidevictor349@gmail.com>
2024-08-25 17:00:34 +0100

    Order table component data

---
+++ data/OrderData.js
@@ -0,0 +1,38 @@
+const orderData = [
+    {
+        "id": 1,
+        name: "Marcus Bergson",
+        date: "Nov 15, 2023",
+        Amount: "80,000",
+        status: "Paid"
+    },
+    {
+        "id": 2,
+        name: "Jaydon VaccaroJaydon Vaccaro",
+        date: "Nov 15, 2023",
+        Amount: "150,000",
+        status: "Refund"
+    },
+    {
+        "id": 1,
+        name: "Corey Schleifer",
+        date: "Nov 15, 2023",
+        Amount: "87,000",
+        status: "Paid"
+    },
+    {
+        "id": 1,
+        name: "Cooper Press",
+        date: "Nov 15, 2023",
+        Amount: "100,000",
+        status: "Refund"
+    },
+    {
+        "id": 1,
+        name: "Phillip Lubin",
+        date: "Nov 15, 2023",
+        Amount: "78,000",
+        status: "Paid"
+    },
+]
+export default orderData;
