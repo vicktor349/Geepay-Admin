e0c1afab18dcf6836ceeb6da7410734943cce70e
vicktor349 <ayomidevictor349@gmail.com>
2024-08-25 17:01:00 +0100

    Added order table and top platform component

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import Navbar from "@/components/Navbar";
 import SideNavbar from "@/components/SideNavbar";
 import Sales from "@/components/Sales";
 import Sale from "@/components/Sale";
-import { OrderTable } from "@/components/OrderTable";
+import OrderTable from "@/components/OrderTable";
+import TopPlatform from "@/components/TopPlatform";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,15 +19,15 @@ export default function Home()
       <main className={`${inter.className} relative`}>
         <Navbar />
         <SideNavbar />
-        <div className="absolute ssm:left-[8px] sssm:left-[8px] ssms:left-[10px] sm:left-[90px] md:left-[100px] lg:left-[110px] xl:left-[80px] top-36 xl:w-[calc(100%-80px)] flex justify-center flex-col">
+        <div className="absolute ssm:left-[8px] sssm:left-[8px] ssms:left-[10px] sm:left-[90px] md:left-[100px] lg:left-[110px] xl:left-[80px] top-36 xl:w-[calc(100%-80px)] flex flex-col">
           <div className="flex justify-center w-full flex-col xl:flex-row xl:space-x-4">
             <Sales />
             <Sale />
           </div>
-          {/* <div className="flex justify-center w-full flex-col xl:flex-row xl:space-x-6">
+          <div className="flex ml-52 flex-col xl:flex-row xl:space-x-4 mt-4">
             <OrderTable />
-            <OrderTable />
-          </div> */}
+            <TopPlatform />
+          </div>
         </div>
       </main>
     </>
